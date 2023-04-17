3df72f3d607f4724c1afdcdaf81bc7a69d529471
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-04-17 16:01:48 +0600

    done nav

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Logo from '../../images/Logo.svg'
+import './Nav.css'
 
 const Nav = () => {
     return (
-        <nav>
+        <nav className='header'>
             <img src={Logo} alt="" />
+            <div className="menu">
+                <a href="">Order</a>
+                <a href="">Order Review</a>
+                <a href="">Inventory</a>
+                <a href="">Login</a>
+            </div>
         </nav>
     );
 };
